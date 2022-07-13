830f11a3f7bb56f64d020b9ba0173ebf7e255feb
vladover1 <anonim.rab@icloud.com>
2022-07-13 21:15:50 +0300

    second

---
+++ source/static/main.js
@@ -0,0 +1,21 @@
+$(() => {
+	$('.top-slider').slick({
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {}
+			},
+			{
+				breakpoint: 600,
+				settings: {}
+			},
+			{
+				breakpoint: 480,
+				settings: {}
+			}
+		]
+	});
+});
